9ccb4365fbb82946600b1d4063e4b20cc52e4368
gabriellaZana <zanagabriella@gmail.com>
2018-04-10 10:45:01 +0200

    modal for registration and login

--- src/main/java/com/codecool/sportSite/controller/SiteController.java
+++ src/main/java/com/codecool/sportSite/controller/SiteController.java
@@ -3,6 +3,7 @@ package com.codecool.sportSite.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
 
 @Controller
 public class SiteController {
