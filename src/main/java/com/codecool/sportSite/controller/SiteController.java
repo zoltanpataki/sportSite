ec8ed16e07601be8cd997ed0b3f27ab40ecfa507
zoltanpataki <zolee7777@gmail.com>
2018-04-12 12:59:36 +0200

    fixing

--- src/main/java/com/codecool/sportSite/controller/SiteController.java
+++ src/main/java/com/codecool/sportSite/controller/SiteController.java
@@ -1,14 +1,17 @@
 package com.codecool.sportSite.controller;
 
+import com.codecool.sportSite.model.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
 @Controller
 public class SiteController {
+
     @GetMapping(value = "/")
-    public String indexView() {
+    public String indexView(@ModelAttribute("user") User user) {
         return "index";
     }
 }
--- src/main/java/com/codecool/sportSite/controller/UserController.java
+++ src/main/java/com/codecool/sportSite/controller/UserController.java
@@ -6,12 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
+import java.util.Map;
 
 import static sun.plugin2.util.PojoUtil.toJson;
 
@@ -29,14 +27,16 @@ public class UserController {
     }
 
     @PostMapping(value="/register")
-    public String registerUser(@RequestBody User user) throws IOException {
+    public String registerUser(@RequestParam Map<String, String> body, User user) throws IOException {
+        System.out.println("reg started");
         ResponseEntity<String> response = apiService.postJson(REGISTER_URL, toJson(user));
         HttpStatus status = response.getStatusCode();
         String restCall = response.getBody();
+        System.out.println(restCall);
 
         if (status == HttpStatus.OK) {
             return "Success. Body: " + restCall;
         }
-        return "Error";
+        return "index";
     }
 }
