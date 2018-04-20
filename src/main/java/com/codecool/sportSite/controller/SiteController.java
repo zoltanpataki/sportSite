1b55b300bbfcf7c10b68ec87ef63928ed289cfa8
zoltanpataki <zolee7777@gmail.com>
2018-04-20 16:34:07 +0200

    sportSiteQuotes is linked to the central app

--- src/main/java/com/codecool/sportSite/controller/SiteController.java
+++ src/main/java/com/codecool/sportSite/controller/SiteController.java
@@ -1,17 +1,30 @@
 package com.codecool.sportSite.controller;
 
 import com.codecool.sportSite.model.User;
+import com.codecool.sportSite.service.ApiService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.SessionAttributes;
+
+import java.util.Map;
 
 @Controller
 public class SiteController {
 
+    @Autowired
+    ApiService apiService;
+
+    private static final String QUOTE_URL = "http://localhost:60002/new-quote";
+
     @GetMapping(value = "/")
-    public String indexView(@ModelAttribute("user") User user) {
+    public String indexView(@ModelAttribute("user") User user, Model model) {
+        Map<String, String> quoteData = apiService.getQuote(QUOTE_URL);
+        System.out.println(quoteData.get("quoteText"));
+        model.addAttribute("newquote", quoteData.get("quoteText"));
+        System.out.println(quoteData.get("quoteAuthor"));
+        model.addAttribute("quoteauthor", quoteData.get("quoteAuthor"));
         return "index";
     }
 }
--- src/main/java/com/codecool/sportSite/service/ApiService.java
+++ src/main/java/com/codecool/sportSite/service/ApiService.java
@@ -1,5 +1,6 @@
 package com.codecool.sportSite.service;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.json.JSONObject;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
@@ -9,6 +10,9 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 public class ApiService {
@@ -27,6 +31,16 @@ public class ApiService {
         HttpEntity<String> entity = new HttpEntity<String>(requestJson,headers);
         System.out.println(entity.getBody());
         return restTemplate.postForEntity(url, entity, String.class);
+    }
+
+    public Map<String, String> getQuote(String url){
+        ObjectMapper mapper = new ObjectMapper();
+        try {
+            return mapper.readValue(new URL(url), HashMap.class);
+        } catch (IOException e){
+            System.out.println("There is no quote service!");
+            return new HashMap<>();
+        }
     }
 }
 
